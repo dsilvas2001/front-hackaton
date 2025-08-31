b94063337acbbc96312f58fe1b909c3a029be5b1
dsilvas2001 <dsilvas@uteq.edu.ec>
2025-08-30 20:35:08 -0500

    interfaz login and register module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'Auth',
+
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
