c5ba1cf29b84767e3efc6e3630c37a5c8c01c6b0
dsilvas2001 <dsilvas@uteq.edu.ec>
2025-08-30 20:06:15 -0500

    interfaz login and register

---
+++ src/app/modules/auth/auth-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import path from 'path';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'sign-in',
+        component: SignInComponent,
+      },
+      {
+        path: 'sign-up',
+        component: SignUpComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'sign-in',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
